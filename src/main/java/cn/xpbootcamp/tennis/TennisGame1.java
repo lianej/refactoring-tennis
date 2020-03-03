589f8ee9b994ed4614a89a614c876cdcd2a9c1d8
wjyan <wjyan@thoughtworks.com>
2020-03-03 11:19:07 +0800

    [tennis]  抽取4分之前的格式化逻辑

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -10,7 +10,7 @@ public class TennisGame1 implements TennisGame {
     private int player2Score;
     private String player2Name;
 
-    String[] scoreMapping = new String[]{"Love","Fifteen","Thirty","Forty"};
+    static String[] scoreMapping = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -26,18 +26,9 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        if (player1Score == player2Score) {
-            switch (player1Score) {
-                case 0:
-                    return "Love-All";
-                case 1:
-                    return "Fifteen-All";
-                case 2:
-                    return "Thirty-All";
-                default:
-                    return "Deuce";
-            }
-        } else if (player1Score >= 4 || player2Score >= 4) {
+        if (player1Score < 4 && player2Score < 4) {
+            return formatScoreBeforeScoreBothLessThan4();
+        } else {
             int minusResult = player1Score - player2Score;
             if (minusResult == 1) {
                 return "Advantage " + player1Name;
@@ -46,12 +37,28 @@ public class TennisGame1 implements TennisGame {
                     return "Advantage " + player2Name;
                 } else if (minusResult >= 2) {
                     return "Win for " + player1Name;
-                } else {
+                } else if (minusResult <= -2) {
                     return "Win for " + player2Name;
+                } else {
+                    return "Deuce";
                 }
             }
-        } else {
-            return String.format("%s-%s",scoreMapping[player1Score],scoreMapping[player2Score]);
+        }
+    }
+
+    private String formatScoreBeforeScoreBothLessThan4() {
+        String scoreText = String.format("%s-%s", scoreMapping[player1Score], scoreMapping[player2Score]);
+        switch (scoreText) {
+            case "Love-Love":
+                return "Love-All";
+            case "Fifteen-Fifteen":
+                return "Fifteen-All";
+            case "Thirty-Thirty":
+                return "Thirty-All";
+            case "Forty-Forty":
+                return "Deuce";
+            default:
+                return scoreText;
         }
     }
 }
