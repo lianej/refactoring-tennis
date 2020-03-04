ca7e23dacd134e33278e9c4c9e93a6133b0a3233
wjyan <wjyan@thoughtworks.com>
2020-03-04 13:54:16 +0800

    [tennis-game2]  抽取Player类

--- src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
@@ -1,17 +1,15 @@
 package cn.xpbootcamp.tennis;
 
 public class TennisGame2 implements TennisGame {
-    public String player1Name;
-    public String player2Name;
-    public int player1Point = 0;
-    public int player2Point = 0;
+    public final Player player1 = new Player();
+    public final Player player2 = new Player();
 
     private String scoreText;
 
 
     public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1.playerName = player1Name;
+        this.player2.playerName = player2Name;
     }
 
     public String getScore() {
@@ -26,12 +24,12 @@ public class TennisGame2 implements TennisGame {
     }
 
     private void ifDeuce() {
-        if (player1Point == player2Point) {
-            if (player1Point == 0) {
+        if (player1.playerPoint == player2.playerPoint) {
+            if (player1.playerPoint == 0) {
                 this.scoreText = "Love-All";
-            } else if (player1Point == 1) {
+            } else if (player1.playerPoint == 1) {
                 this.scoreText = "Fifteen-All";
-            } else if (player1Point == 2) {
+            } else if (player1.playerPoint == 2) {
                 this.scoreText = "Thirty-All";
             } else {
                 this.scoreText = "Deuce";
@@ -40,37 +38,42 @@ public class TennisGame2 implements TennisGame {
     }
 
     private void ifProgressing() {
-        if (player1Point == player2Point || player1Point > 3 || player2Point > 3) {
+        if (player1.playerPoint == player2.playerPoint || player1.playerPoint > 3 || player2.playerPoint > 3) {
             return;
         }
         String[] mapping = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-        this.scoreText = String.format("%s-%s", mapping[player1Point], mapping[player2Point]);
+        this.scoreText = String.format("%s-%s", mapping[player1.playerPoint], mapping[player2.playerPoint]);
 
     }
 
     private void ifAdvantage() {
-        if (player1Point > player2Point && player2Point >= 3) {
-            this.scoreText = "Advantage " + player1Name;
+        if (player1.playerPoint > player2.playerPoint && player2.playerPoint >= 3) {
+            this.scoreText = "Advantage " + player1.playerName;
         }
 
-        if (player2Point > player1Point && player1Point >= 3) {
-            this.scoreText = "Advantage " + player2Name;
+        if (player2.playerPoint > player1.playerPoint && player1.playerPoint >= 3) {
+            this.scoreText = "Advantage " + player2.playerName;
         }
     }
 
     private void ifWin() {
-        if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) {
-            this.scoreText = "Win for " + player1Name;
+        if (player1.playerPoint >= 4 && player2.playerPoint >= 0 && (player1.playerPoint - player2.playerPoint) >= 2) {
+            this.scoreText = "Win for " + player1.playerName;
         }
-        if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2) {
-            this.scoreText = "Win for " + player2Name;
+        if (player2.playerPoint >= 4 && player1.playerPoint >= 0 && (player2.playerPoint - player1.playerPoint) >= 2) {
+            this.scoreText = "Win for " + player2.playerName;
         }
     }
 
     public void wonPoint(String player) {
-        if (player.equals(player1Name))
-            player1Point++;
+        if (player.equals(player1.playerName))
+            player1.playerPoint++;
         else
-            player2Point++;
+            player2.playerPoint++;
+    }
+
+    private static class Player {
+        public String playerName;
+        public int playerPoint = 0;
     }
 }
