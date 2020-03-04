59bb85f4c03cbde6a3d4b276107aee9e648a4397
wjyan <wjyan@thoughtworks.com>
2020-03-04 14:28:21 +0800

    [tennis-game3] clear code

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -1,36 +1,39 @@
 package cn.xpbootcamp.tennis;
 
+import java.util.Objects;
+
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int point1;
+    private String player1Name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private int point2;
+    private String player2Name;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (point1 < 4 && point2 < 4 && !(point1 + point2 == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[point1];
+            return (point1 == point2) ? s + "-All" : s + "-" + p[point2];
         } else {
-            if (p1 == p2)
+            if (point1 == point2)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = point1 > point2 ? player1Name : player2Name;
+            return ((point1 - point2)*(point1 - point2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        if (Objects.equals(playerName, player1Name))
+            this.point1 += 1;
         else
-            this.p2 += 1;
+            this.point2 += 1;
 
     }
 
